7f6a30f83d69cf5ef484a58068536da94b100b77
Nicolas <nicolas.loiseau@epitech.eu>
2023-03-04 12:04:52 -0500

    [ADD] generator

--- generator/generator.c
+++ generator/generator.c
@@ -5,31 +5,57 @@
 ** generator
 */
 
-#include "../include/my.h"
+#include <time.h>
+#include <stdio.h>
+#include "../solver/include/solver.h"
 
-static void display_nb(void)
+int is_safe(int **grid, int row, int col, int num)
 {
-    int *arr =  malloc(sizeof(int *) * 9);
+    int row_start = row - (row % 3);
+    int col_start = col - (col % 3);
 
-    for (int i = 0; i < rand() % 4; i++) {
-            arr[i] = rand() % (9 - 1) + 1;
+    for (int i = 0; i <= N - 1; ++i) {
+        if (grid[row][i] == num)
+            return (0);
+        if (grid[i][col] == num)
+            return (0);
+        if (grid[row_start + (i % 3)][col_start + (i / 3)] == num)
+            return (0);
     }
+    return (1);
+}
 
-
-    for (int i = 0; i < 9; i++) {
-        printf((arr[i] != 0) ? "%d" : " ", arr[i]);
-        printf(" ");
+int **display_nb(int **arr, int column, int row)
+{
+    if (column == 8) {
+        column = 0;
+        return (display_nb(arr, column, row + 1));
     }
+    for (int j = 0; j < 9; j++) {
+        if (is_safe(arr, row, column, j) == 1) {
+            arr[row][column] = j;
+            if (display_nb(arr, column++, row));
+            return(1);
+        } else {
+            arr[column][j] = 0;
+        }
+    }
+    return (0);
 }
 
 static void display_map(void)
 {
-    printf("|------------------|\n");
+    int **arr = malloc(sizeof(int *) * 9);
+    int column = 0, row = 0;
     for (int i = 0; i < 9; i++) {
-        printf("|");
-        display_nb();
-        printf("|\n");
+        arr[i] = malloc(sizeof(int) * 9);
+        for (int j = 0; j < 9; j++) {
+            arr[i][j] = 0;
+        }
     }
+
+    printf("|------------------|\n");
+    display_nb(arr, column, row);
     printf("|------------------|\n");
 }
 
